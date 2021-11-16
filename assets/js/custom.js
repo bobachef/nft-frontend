0e10833b2cbfb5bd184f505ec5219664d58e81b1
shaharyar saleem <shaharyarsaleem71@gmail.com>
2021-11-16 19:32:47 +0500

    style: run prettier

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,11 @@
 // Scroll effect on navbar
 $(window).scroll(function () {
-    if ($(window).scrollTop() >= 330) {
-      $("nav").addClass("fixed-header");
-      $("nav").css("background", "#0f0f1482");
-      $("nav").css("transition", "max-height 0.5s ease 0s");
-    } else {
-      $("nav").removeClass("fixed-header");
-      $("nav").css("background", "none");
-    }
-  });
+  if ($(window).scrollTop() >= 330) {
+    $("nav").addClass("fixed-header");
+    $("nav").css("background", "#0f0f1482");
+    $("nav").css("transition", "max-height 0.5s ease 0s");
+  } else {
+    $("nav").removeClass("fixed-header");
+    $("nav").css("background", "none");
+  }
+});
