f3837a19b19e346b286dfe305d2491d39247cd83
Boba Chef <82844481+bobachef@users.noreply.github.com>
2021-12-14 15:44:35 -0800

    Webpack prod cc1 (#19)
    * ft: webpack for production
    
    * ft: add imagemin
    
    * style: formatting

---
+++ webpack.common.js
@@ -0,0 +1,96 @@
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.json$/,
+        loader: "json-loader",
+      },
+      {
+        test: /\.html$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "index.html",
+        },
+      },
+      // {
+      //   test: /\.html$/i,
+      //   loader: "html-loader",
+      // },
+      // {
+      // test: '/\\/$|index\\.html/',
+      // type: "asset/resource",
+      // generator: {
+      //   filename: 'index.html'
+      // },
+      // },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        type: "asset",
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "img",
+          to: "img",
+        },
+        {
+          from: "css",
+          to: "css",
+        },
+        // {
+        //   context: path.resolve(__dirname, "dist"),
+        //   from: "./*.html",\
+        // },
+      ],
+    }),
+  ],
+  optimization: {
+    minimize: false,
+    minimizer: [
+      "...",
+      new TerserPlugin(),
+      new HtmlMinimizerPlugin(),
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.squooshMinify,
+          options: {
+            encodeOptions: {
+              mozjpeg: {
+                // That setting might be close to lossless, but it’s not guaranteed
+                // https://github.com/GoogleChromeLabs/squoosh/issues/85
+                quality: 100,
+              },
+              webp: {
+                lossless: 1,
+              },
+              avif: {
+                // https://github.com/GoogleChromeLabs/squoosh/blob/dev/codecs/avif/enc/README.md
+                cqLevel: 0,
+              },
+            },
+          },
+        },
+      }),
+    ],
+  },
+  resolve: {
+    modules: ["node_modules"],
+    fallback: [],
+  },
+  entry: ["./js/index.js", "./index.html"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "nft.bundle.js",
+    clean: true,
+  },
+};
